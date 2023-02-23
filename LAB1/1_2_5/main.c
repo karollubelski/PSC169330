4df95ee4b8e0964b50d5ba033d6e11b19e860f8b
daesxsap <>
2023-02-23 15:56:50 +0100

    n

---
+++ LAB1/1_2_5/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    double liczba;
+    scanf("%lf", &liczba);
+    printf("Liczba wymierna:%lf\n", liczba);
+
+
+}
---
+++ LAB1/1_2_6/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int liczba1,liczba2,liczba3;
+    scanf("%d", &liczba1);
+    scanf("%d", &liczba2);
+    scanf("%d", &liczba3);
+    printf("Liczba pierwsza: %d\n",liczba1);
+    printf("Liczba druga: %d\n",liczba2);
+    printf("Liczba trzecia: %d\n",liczba3);
+
+
+
+}
