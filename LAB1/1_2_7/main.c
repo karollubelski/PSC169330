69a7e2826abe4fe8ae9e6e3b52e70d638934a97b
daesxsap <>
2023-02-23 16:02:38 +0100

    1_2_7

---
+++ LAB1/1_2_7/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x;
+    printf("Podaj liczbe calkowita: ");
+    scanf("%d",&x);
+    printf("Liczba o 1 wieksza od podanej to: %d\n",x+1);
+    return 0;
+}
