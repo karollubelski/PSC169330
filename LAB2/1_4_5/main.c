a66575e7544a8334836c1af9034ca03afd8a82d4
karollubelski <115708844+karollubelski@users.noreply.github.com>
2023-03-05 21:54:22 +0100

    n

---
+++ LAB2/1_4_5/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   unsigned int n,m,k;
+   printf("Podaj n: ");
+   scanf("%u",&n);
+
+   int suma;
+   for(int i=0;i<=n;i++){
+        suma+=i*i;
+   }
+   printf("Suma kwadratow liczb od 0 do n wynosi: %d", suma);
+
+
+
+
+    return 0;
+
+}
