c2e44707a27b8dc33eb6ff2366f99436edf8827f
karollubelski <115708844+karollubelski@users.noreply.github.com>
2023-03-09 22:55:22 +0100

    n

---
+++ LAB3/2_2_24/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int funkcja(int n){
+    if(n==0 || n==1){
+        return 1;
+    }else{
+    int suma=0;
+    for(int i=0;i<n;i++){
+        suma+=funkcja(i);
+    }
+    return suma;
+    }
+}
+
+int main()
+{
+    printf("Wynik dla n=4: %d\n",funkcja(4));
+    printf("Wynik dla n=5: %d\n",funkcja(5));
+    printf("Wynik dla n=1: %d\n",funkcja(1));
+    printf("Wynik dla n=3: %d\n",funkcja(3));
+}
