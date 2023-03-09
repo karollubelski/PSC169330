8174c943de5f4aff53bcf1bfb11c45e2f49c0e79
karollubelski <115708844+karollubelski@users.noreply.github.com>
2023-03-09 13:09:01 +0100

    n

---
+++ LAB3/2_2_2/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int silnia(n){
+    if(n==0){
+        return 1;
+    }else{
+    return n*silnia(n-1);
+    }
+}
+
+int main()
+{
+    printf("Silnia z 5: %d\n",silnia(5));
+    printf("Silnia z 5: %d\n",silnia(1));
+    printf("Silnia z 5: %d\n",silnia(6));
+}
