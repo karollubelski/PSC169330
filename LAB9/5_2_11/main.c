91dcabffbb4fdf5650b7b878fb68aa37810619e6
karollubelski <115708844+karollubelski@users.noreply.github.com>
2023-05-11 00:29:02 +0200

    n

--- LAB9/5_2_11/main.c
+++ LAB9/5_2_11/main.c
@@ -1,45 +1,44 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 
-void wytnijzw(char* napis1, char* napis2){
-    int dl1=strlen(napis1);
-    int dl2=strlen(napis2);
-    int pom=0;
-    for(int i=0;i<dl1;i++){
-        int j;
-        for(j=0;j<dl2;j++){
-            if(napis1[i]==napis2[i]){
+void wytnijzw(char* nap1,const char* nap2){
+    int i,j,k;
+    int n=strlen(nap2);
+    for(i=0;nap1[i]!='\0';){
+        for(j=0;j<n;j++){
+            if(nap1[i]==nap2[j]){
+                for(k=i;nap1[k]!='\0';k++){
+                    nap1[k]=nap1[k+1];
+                }
                 break;
             }
         }
-        if(j==dl2){
-            napis1[pom++]=napis1[i];
+        if (j==n){
+            i++;
         }
     }
-    napis1[pom]=0;
 }
-void wytnijzw2(wchar_t* napis1, wchar_t* napis2){
-    int dl1=wcslen(napis1);
-    int dl2=wcslen(napis2);
-    int pom=0;
-    for(int i=0;i<dl1;i++){
-        int j;
-        for(j=0;j<dl2;j++){
-            if(napis1[i]==napis2[i]){
+void wytnijzw2(wchar_t* nap1,const wchar_t* nap2){
+    int i,j,k;
+    int n=wcslen(nap2);
+    for(i=0;nap1[i]!= L'\0';){
+        for(j=0;j<n;j++){
+            if(nap1[i]==nap2[j]){
+                for(k=i;nap1[k]!=L'\0';k++){
+                    nap1[k]=nap1[k+1];
+                }
                 break;
             }
         }
-        if(j==dl2){
-            napis1[pom++]=napis1[i];
+        if(j==n){
+            i++;
         }
     }
-    napis1[pom]=0;
 }
 int main()
 {
-    char napis1[]="MercedesAmgGtr";
-    char napis2[]="mercedesamggtr"; //????????????
+    char napis1[]="ducati";
+    char napis2[]="mercedes";
     wytnijzw(napis1,napis2);
     printf("%s\n",napis1);
 }
