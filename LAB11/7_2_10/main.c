bdc9532bfa2f7709ce4ef011993e269ef2deeae9
asdf <115708844+karollubelski@users.noreply.github.com>
2023-05-11 15:58:20 +0200

    n

--- LAB11/7_2_10/main.c
+++ LAB11/7_2_10/main.c
@@ -1,35 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-union Liczba
-{
-    int calkowita;
-    double wymierna;
+union super_int{
+    int a;
+    unsigned int b;
 };
-struct dane
-{
-    int tp;
-    union liczba zaw;
-};
-struct dane wczytajdane()
-{
-    struct dane data;
-    printf("Jezeli chcesz liczbe calkowita wpisz liczbe 0 nastepnie enter\n");
-    printf("Jezeli chcesz liczbe wymierna wpisz liczbe 1 nastepnie enter\n");
-    scanf("%d",&data.tp);
-    if(data.tp==0)
-    {
-        printf("Wpisz swoja liczbe calkowita");
-        scanf("%d",&data.zaw.calkowita);
-    }
-    else
-    {
-        printf("Wpisz swoja liczbe wymierna");
-        scanf("%d",&data.zaw.wymierna);
-    }
-    return data;
-}
 int main()
 {
-    wczytajdane();
-    return 0;
+    union super_int zmienna;
+    zmienna.a=4;
+    printf("%d\n",zmienna.a);
+    printf("%u\n",zmienna.a);
+    printf("---\n");
+    zmienna.b=4;
+    printf("%d\n",zmienna.a);
+    printf("%u\n",zmienna.a);
+    printf("---\n");
+    zmienna.a=-4;
+    printf("%d\n",zmienna.a);
+    printf("%u\n",zmienna.a);
+    printf("---\n");
+    zmienna.b=-4;
+    printf("%d\n",zmienna.a);
+    printf("%u\n",zmienna.a);
+    printf("---\n");
 }
---
+++ LAB11/7_2_15/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+enum zwierzak{PIES=-3, KOT, KURA, KOGUT};
+
+int main()
+{
+    enum zwierzak zmienna = KOT;
+    printf("%d\n",zmienna);
+    printf("%Iu\n",sizeof(enum zwierzak));
+}
